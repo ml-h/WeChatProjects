ad728069bd8e057faaf5782c2b22c84d89bb23c1
筱 <oncwnuBLweuO3AhUVLNc7HurrdrI@git.weixin.qq.com>
2020-11-12 23:15:41 +0800

    README.md

---
+++ miniprogram/components/search/search.js
@@ -0,0 +1,35 @@
+// components/search/search.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    placeholder:{
+      type : String,
+      value : '  请输入关键词搜索试题'
+    }
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+    searchclick :function(e){
+      wx.navigateTo({
+        url: '../../pages/souResult/souResult',
+      })
+    },
+
+    
+  }
+})
