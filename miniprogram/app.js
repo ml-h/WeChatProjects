d69bc951ae8dbea1f4a3ce2ef36dab6555306884
blh506 <2366535842@qq.com>
2020-11-13 23:13:54 +0800

    测试社区2.0

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -11,7 +11,8 @@ App({
         //   env 参数决定接下来小程序发起的云开发调用（wx.cloud.xxx）会默认请求到哪个云环境的资源
         //   此处请填入环境 ID, 环境 ID 可打开云控制台查看
         //   如不填则使用默认环境（第一个创建的环境）
-        env: 'test-yan-3gp1h1ez7f7c6a02',
+        //env: 'test-yan-3gp1h1ez7f7c6a02',
+        env: 'blh-0grxkpcj3b85c6a8',
         traceUser: true,
       })
     }
