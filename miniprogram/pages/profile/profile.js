e117b857b664e508d23306c22f42f157799cc1bb
muyu-sg <1563467062@qq.com>
2020-11-14 15:37:17 +0800

    设置授权页面

--- miniprogram/pages/profile/profile.js
+++ miniprogram/pages/profile/profile.js
@@ -28,8 +28,6 @@ Page({
     if (app.globalData.userInfo) {
       that.setUserInfo(app.globalData.userInfo);
     } else if (that.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
         that.setUserInfo(res.userInfo);
       }
