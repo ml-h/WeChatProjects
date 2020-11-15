cee9452d91a8db2578c78a5d20daab77574d7559
xiaoxxiao <2642908922@qq.com>
2020-11-15 12:59:19 +0800

    修改题目详情页面

--- miniprogram/pages/Tidetails/Tidetails.js
+++ miniprogram/pages/Tidetails/Tidetails.js
@@ -1,34 +1,31 @@
-// pages/souResult/souResult.js
-const db=wx.cloud.database()
-
+// pages/Tidetails/Tidetails.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    dataObj:"",
-    
-  },
-  
-  
-  getData(){
-    db.collection("Tilist").get({
-      success:res=>{
-        // console.log("题目集")
-        // console.log(res.data)
-        this.setData({
-          dataObj:res.data
-        })
-      }
-    })
+    detail:''
 
   },
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.getData()
+    console.log("详情页接收的id",options.id)
+    wx.cloud.database().collection("Tilist")
+    .doc(options.id)
+    .get()
+    .then(res=>{
+      console.log("详情页成功",res)
+      this.setData({
+        detail:res.data
+      })
+    })
+    .catch(res=>{
+      console.log("详情页失败",res)
+    })
 
   },
 
@@ -79,14 +76,6 @@ Page({
    */
   onShareAppMessage: function () {
 
-  },
-  detailclick(event){
-    console.log("点击获取的数据",event.currentTarget.dataset.item._id)
-    wx.navigateTo({
-      url: '../../pages/Tidetails/Tidetails?id='+event.currentTarget.dataset.item._id,
-    })  
-    
- 
-    
   }
+
 })
