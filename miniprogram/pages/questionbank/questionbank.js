aacd6bfdb63ce92b040f8b442320cc9fb3e027f1
muyu-sg <1563467062@qq.com>
2020-12-22 14:39:17 +0800

    合并

--- miniprogram/pages/questionbank/questionbank.js
+++ miniprogram/pages/questionbank/questionbank.js
@@ -15,12 +15,13 @@ Page({
   // }else{
     // console.log("跳转页面",event.target.dataset.course)
     // 跳转到页面 zhenti,并携带参数
-    const userInfo=event.detail.userInfo;
-    if(userInfo){
+    console.log(event)
+    // const userInfo=event.detail.userInfo;
+    // if(userInfo){
     wx.navigateTo({
       url:'../zhenti/zhenti?type='+event.currentTarget.dataset.type+'&course='+event.currentTarget.dataset.course,
     })
-  }
+  // }
   },
   
   changepage3:function(event){
@@ -47,6 +48,26 @@ Page({
     wx.navigateTo({
       url:'../feitongkao/feitongkao',
     })}
+  },
+  input: function(e) {//接受输入值
+    this.setData({
+      key: e.detail.value
+    })
+  },
+
+  searchclick :function(e){
+    if(this.data.key.length>0){
+      wx.navigateTo({
+        url: '../../pages/souResult/souResult?key='  + this.data.key,
+      })  
+    }else{
+      console.log("不支持无关键词搜索")
+      wx.showToast({
+        title: "请输入关键词",
+        duration:2000,
+        icon:'none'
+      })
+    }
   }
 
 })
